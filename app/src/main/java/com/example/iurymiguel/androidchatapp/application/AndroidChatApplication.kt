6145363c1d9b2db98c3d9318336006837354068c
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2018-12-29 16:31:44 -0200

    Creating subjects fragments

---
+++ app/src/main/java/com/example/iurymiguel/androidchatapp/application/AndroidChatApplication.kt
@@ -0,0 +1,10 @@
+package com.example.iurymiguel.androidchatapp.application
+
+import android.app.Application
+
+class AndroidChatApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/example/iurymiguel/androidchatapp/views/subjects/MainActivity.kt
+++ app/src/main/java/com/example/iurymiguel/androidchatapp/views/subjects/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.iurymiguel.androidchatapp.views
+package com.example.iurymiguel.androidchatapp.views.subjects
 
 import android.support.design.widget.TabLayout
 import android.support.design.widget.Snackbar
@@ -8,15 +8,11 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
 import com.example.iurymiguel.androidchatapp.R
 
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -72,62 +68,18 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-
-    /**
-     * A [FragmentPagerAdapter] that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
+        private val twoFragments = 2
+
         override fun getItem(position: Int): Fragment {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(
-                position + 1
-            )
-        }
-
-        override fun getCount(): Int {
-            // Show 2 total pages.
-            return 2
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    class PlaceholderFragment : Fragment() {
-
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-            rootView.section_label.text = getString(
-                R.string.section_format, arguments?.getInt(
-                    ARG_SECTION_NUMBER
-                ))
-            return rootView
-        }
-
-        companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
-            private val ARG_SECTION_NUMBER = "section_number"
-
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
-            fun newInstance(sectionNumber: Int): PlaceholderFragment {
-                val fragment = PlaceholderFragment()
-                val args = Bundle()
-                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
-                fragment.arguments = args
-                return fragment
+            return when(position) {
+                1 -> SubscribedSubjectsFragment.newInstance()
+                else -> UnsubscribedSubjectsFragment.newInstance()
             }
+        }
+        override fun getCount(): Int {
+            return twoFragments
         }
     }
 }
