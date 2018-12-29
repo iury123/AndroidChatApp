98d1adca6a70eaba290f205d1c91c99993c79e20
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2018-12-29 18:09:17 -0200

    Implementing sign in fragment

--- app/src/main/java/com/example/iurymiguel/androidchatapp/views/subjects/MainActivity.kt
+++ app/src/main/java/com/example/iurymiguel/androidchatapp/views/subjects/MainActivity.kt
@@ -23,11 +23,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
 
-        // Set up the ViewPager with the sections adapter.
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+        
         viewPager.adapter = mSectionsPagerAdapter
 
         viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
