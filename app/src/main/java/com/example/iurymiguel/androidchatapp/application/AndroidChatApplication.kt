205fd8ec84735a1cf8790a9c674efde724188303
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2018-12-31 18:05:43 -0200

    Deleting pointless code

--- app/src/main/java/com/example/iurymiguel/androidchatapp/application/AndroidChatApplication.kt
+++ app/src/main/java/com/example/iurymiguel/androidchatapp/application/AndroidChatApplication.kt
@@ -3,21 +3,16 @@ package com.example.iurymiguel.androidchatapp.application
 import android.app.Application
 import android.content.Intent
 import com.example.iurymiguel.androidchatapp.utils.ProgressDialogProvider
-import com.example.iurymiguel.androidchatapp.utils.SharedPreferencesProvider
-import com.example.iurymiguel.androidchatapp.views.authentication.AuthenticationActivity
 import com.example.iurymiguel.androidchatapp.views.subjects.MainActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
-import org.koin.android.ext.android.get
 import org.koin.android.ext.android.startKoin
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module.module
 
 class AndroidChatApplication : Application() {
 
     private val appModule = module {
         single { ProgressDialogProvider() }
-        single { SharedPreferencesProvider(androidContext()) }
     }
 
     override fun onCreate() {
